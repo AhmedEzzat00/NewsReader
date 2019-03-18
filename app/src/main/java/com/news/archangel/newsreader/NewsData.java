fb752195906ab23ce9bb154b6af4577a47143cc4
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2019-03-18 21:45:31 +0200

    Create NewsData
    initialize the NewsData class in order to store the data coming from the news API

---
+++ app/src/main/java/com/news/archangel/newsreader/NewsData.java
@@ -0,0 +1,57 @@
+package com.news.archangel.newsreader;
+
+public class NewsData {
+    private String title;
+    private String description;
+    private String urlToNews;
+    private String urlToImage;
+    private String time;
+
+    public NewsData(String title, String description, String urlToNews, String urlToImage, String time) {
+        this.title = title;
+        this.description = description;
+        this.urlToNews = urlToNews;
+        this.urlToImage = urlToImage;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlToNews() {
+        return urlToNews;
+    }
+
+    public void setUrlToNews(String urlToNews) {
+        this.urlToNews = urlToNews;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
