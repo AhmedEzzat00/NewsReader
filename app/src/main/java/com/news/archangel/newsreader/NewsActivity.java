3159c293663b6123d66e71606de9a8a4750e42d9
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2019-03-22 19:55:39 +0200

    Create search menu

--- app/src/main/java/com/news/archangel/newsreader/NewsActivity.java
+++ app/src/main/java/com/news/archangel/newsreader/NewsActivity.java
@@ -1,12 +1,19 @@
 package com.news.archangel.newsreader;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,6 +67,18 @@ public class NewsActivity extends AppCompatActivity {
             recyclerList.setAdapter(adapter);
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+      /*  SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        */
+        return true;
     }
 }
 
