ee3179b2f77fcc672f5931c26675a13204e8a0da
Ahmed Ezzat <ahmed.ezzat176148@gmail.com>
2019-03-22 21:42:07 +0200

    Add search function

--- app/src/main/java/com/news/archangel/newsreader/NewsActivity.java
+++ app/src/main/java/com/news/archangel/newsreader/NewsActivity.java
@@ -2,6 +2,7 @@ package com.news.archangel.newsreader;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,7 +25,7 @@ public class NewsActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = NewsActivity.class.getName();
     private static final String NEWS_API_REQUEST_URL =
-            "https://newsapi.org/v2/everything?q=bitcoin";
+            "https://newsapi.org/v2/everything?q=sources";
     //This Key is obtained from the site by register on it
     private static final String API_KEY =
             "c3810091c7844f18bf2ee02a762d90a0";
@@ -44,25 +46,35 @@ public class NewsActivity extends AppCompatActivity {
 
         adapter=new NewsAdapter(new ArrayList<Article>());
         recyclerList.setAdapter(adapter);
-        new NewsAysc().execute(NEWS_API_REQUEST_URL);
+        //Building the URL of the API
+        Uri baseUri = Uri.parse(NEWS_API_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+
+        Intent intent = getIntent();
+        String query = "";
+        if (intent.getAction().equals(Intent.ACTION_SEARCH)) {
+            query = intent.getStringExtra(SearchManager.QUERY);
+            if (!TextUtils.isEmpty(query)) {
+                uriBuilder.clearQuery();
+                uriBuilder.appendQueryParameter("q", query);
+            }
+        }
+
+        uriBuilder.appendQueryParameter("apiKey", API_KEY);
+        Log.i(LOG_TAG, query);
+        new NewsAysc().execute(uriBuilder.toString());
     }
 
     private class NewsAysc extends AsyncTask<String, Void, List<Article>> {
 
         @Override
         protected List<Article> doInBackground(String... urls) {
-
-            //Building the URL of the API
-            Uri baseUri = Uri.parse(urls[0]);
-            Uri.Builder uriBuilder = baseUri.buildUpon();
-            uriBuilder.appendQueryParameter("apiKey", API_KEY);
-
-            return NewsUtilities.fetchNewsDataResponse(uriBuilder.toString());
+            return NewsUtilities.fetchNewsDataResponse(urls[0]);
         }
 
         @Override
         protected void onPostExecute(List<Article> articles) {
-
             adapter= new NewsAdapter(articles);
             recyclerList.setAdapter(adapter);
 
@@ -73,11 +85,11 @@ public class NewsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_search, menu);
-      /*  SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         MenuItem searchItem = menu.findItem(R.id.menu_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        */
+
         return true;
     }
 }
